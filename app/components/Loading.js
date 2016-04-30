a4aeca56b7cd1a056e86be27b6728c68741cdd54
Bryan Kendall <bryan@runnable.com>
2016-04-29 19:46:17 -0700

    add loading component

---
+++ app/components/Loading.js
@@ -0,0 +1,34 @@
+import NProgress from 'nprogress'
+import React from 'react'
+
+class Loading extends React.Component {
+  constructor () {
+    super()
+    NProgress.start()
+  }
+
+  componentDidMount () {
+    this.interval = setInterval(() => {
+      NProgress.inc()
+    }, 1000)
+  }
+
+  componentWillUnmount () {
+    clearTimeout(this.interval)
+    NProgress.done()
+  }
+
+  render () {
+    return (
+      <div className='row'>
+        <div className='col-xs-12'>
+          <p className='text-center'>
+            <i className='fa fa-spinner fa-spin fa-3x fa-fw fa-align-center margin-bottom'></i>
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Loading

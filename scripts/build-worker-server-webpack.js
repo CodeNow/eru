1857a0e5a112df2b0b1cf54dcaa64039c2288493
Bryan Kendall <bryan@runnable.com>
2016-05-19 13:05:27 -0700

    new scripts for building servers

---
+++ scripts/build-worker-server-webpack.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const path = require('path')
+const webpack = require('webpack')
+
+// Serve the Relay app
+var compiler = webpack({
+  entry: path.resolve(__dirname, '..', 'index-workers.js'),
+  output: { filename: 'worker-server.bundle.js', path: path.resolve(__dirname, '..') },
+  target: 'node',
+  externals: fs.readdirSync(path.resolve(__dirname, '../node_modules'))
+    .concat('@runnable/orion')
+    .reduce(function (ext, mod) {
+      ext[mod] = 'commonjs ' + mod
+      return ext
+    }, {}),
+  node: {
+    __filename: true,
+    __dirname: true
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        exclude: /node_modules/,
+        test: /\.js$/
+      }
+    ]
+  }
+})
+
+compiler.run((err, stats) => {
+  if (err) { throw err }
+  if (stats && stats.compilation) {
+    if (stats.compilation.errors && stats.compilation.errors.length) {
+      console.log(stats.compilation.errors[0].message)
+      console.log(stats.compilation.errors[0].stack)
+      process.exit(1)
+    }
+  }
+})

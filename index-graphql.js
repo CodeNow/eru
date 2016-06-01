37dd8db09958d35923053ba6d3cfed87fe252b9d
Bryan Kendall <bryan@runnable.com>
2016-06-01 13:03:38 -0700

    connect cache layer singleton

--- index-graphql.js
+++ index-graphql.js
@@ -1,10 +1,14 @@
 import loadenv from 'loadenv'
 loadenv()
 
+import cacheLayer from './data/cache-layer'
 import graphQLServer from './lib/graphql-server'
 
 const { GRAPHQL_PORT } = process.env
 
-graphQLServer.listen(GRAPHQL_PORT, () => {
-  console.log(`App Server running on port ${GRAPHQL_PORT}`)
-})
+cacheLayer.connect()
+  .then(() => {
+    graphQLServer.listen(GRAPHQL_PORT, () => {
+      console.log(`App Server running on port ${GRAPHQL_PORT}`)
+    })
+  })

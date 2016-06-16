4c0ec9e0832585e2684e7c4abd11214d0dbd9756
Bryan Kendall <bryan@runnable.com>
2016-06-16 13:54:13 -0700

    connect cache layer for workers

--- index-workers.js
+++ index-workers.js
@@ -1,8 +1,10 @@
 import loadenv from 'loadenv'
 loadenv()
 
+import cacheLayer from './data/cache-layer'
 import Runnable from './data/runnable'
 import workerServer from './lib/worker-server'
 
 Runnable.connect()
+  .then(() => (cacheLayer.connect()))
   .then(() => (workerServer.start()))

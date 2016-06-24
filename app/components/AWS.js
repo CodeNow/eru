afed344c88b0815fbb9d7edf2330433813ecd6b3
Bryan Kendall <bryan@runnable.com>
2016-06-24 14:17:56 -0700

    add button to filter out empty asgs

--- app/components/AWS.js
+++ app/components/AWS.js
@@ -9,15 +9,35 @@ class AWS extends React.Component {
     alertMessage: React.PropTypes.func.isRequired
   }
 
+  constructor(...args) {
+    super(...args)
+    this.state = { hideZeros: false }
+  }
+
+  _handleHideButtonPress = (ref) => {
+    this.setState({ hideZeros: !this.state.hideZeros })
+  }
+
   render () {
     const {
       alertMessage,
+    } = this.props
+    let {
       runnable: { aws: { asgs } }
     } = this.props
+    if (this.state.hideZeros) {
+      asgs = asgs.filter((a) => (a.desiredSize))
+    }
     return (
       <div className='row'>
         <div className='col-md-12'>
           <h4>ASGs</h4>
+          <button
+            className='btn btn-default'
+            onClick={this._handleHideButtonPress}
+          >
+            { (this.state.hideZeros ? 'Show' : 'Hide') + ' Empty ASGs' }
+          </button>
           <table className='table table-striped table-condensed table-hover'>
             <thead>
               <tr>
@@ -56,6 +76,7 @@ export default Relay.createContainer(
           aws {
             asgs {
               id
+              desiredSize
               ${ASGRow.getFragment('asg')}
             }
           }
